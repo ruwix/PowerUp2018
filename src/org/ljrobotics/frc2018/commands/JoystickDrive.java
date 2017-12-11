40f2b29c08870ea067e96c6bf6b2cf020c236ca3
ruwix <grantpauker@gmail.com>
2017-12-11 15:07:00 -0800

    updated joystick drive

--- src/org/ljrobotics/frc2018/commands/JoystickDrive.java
+++ src/org/ljrobotics/frc2018/commands/JoystickDrive.java
@@ -2,7 +2,7 @@ package org.ljrobotics.frc2018.commands;
 
 import org.ljrobotics.frc2018.subsystems.Drive;
 import org.ljrobotics.frc2018.utils.Motion;
-
+import org.ljrobotics.frc2018.Constants;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Command;
@@ -18,13 +18,13 @@ public class JoystickDrive extends Command {
 
 	// Called just before this Command runs the first time
 	protected void initialize() {
-		this.joystick = new Joystick(1);
+		this.joystick = new Joystick(Constants.JOYSTICK_DRIVE_ID);
 		this.multiplier=1;
 	}
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
-		double y = this.joystick.getRawAxis(1)*this.multiplier;
+		double y = -this.joystick.getRawAxis(1)*this.multiplier;
 		double rotation = this.joystick.getRawAxis(2)*this.multiplier;
 		Drive.getInstance().move(new Motion(y,rotation));
 	}
@@ -37,6 +37,7 @@ public class JoystickDrive extends Command {
 	// subsystems is scheduled to run
 	protected void interrupted() {
 	}
+
 	protected boolean isFinished(){
 		return false;
 	}
