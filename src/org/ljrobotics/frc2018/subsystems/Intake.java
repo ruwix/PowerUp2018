13f9b9168823a0a39ca73dd7e85946e98e180505
ruwix <grantpauker@gmail.com>
2018-01-30 15:09:50 -0800

    update intake subsytem for test and new commands

--- src/org/ljrobotics/frc2018/subsystems/Intake.java
+++ src/org/ljrobotics/frc2018/subsystems/Intake.java
@@ -25,9 +25,13 @@ public class Intake extends Subsystem implements LoopingSubsystem {
 			TalonSRX left = new TalonSRX(Constants.LEFT_INTAKE_MOTOR_ID);
 			TalonSRX right = new TalonSRX(Constants.RIGHT_INTAKE_MOTOR_ID);
 			TalonSRX tension = new TalonSRX(Constants.TENSION_INTAKE_MOTOR_ID);
-			AnalogInput leftDistnace = new AnalogInput(0);
+			AnalogInput leftDistance = new AnalogInput(0);
 			AnalogInput rightDistance = new AnalogInput(1);
-			instance = new Intake(left, right, tension, leftDistnace, rightDistance);
+			// leftDistance.setOversampleBits(4);
+			leftDistance.setAverageBits(4);
+			rightDistance.setAverageBits(4);
+
+			instance = new Intake(left, right, tension, leftDistance, rightDistance);
 		}
 		return instance;
 	}
@@ -47,6 +51,7 @@ public class Intake extends Subsystem implements LoopingSubsystem {
 	private double wantedLeftSpeed;
 	private double wantedRightSpeed;
 	private double wantedTensionPower;
+	private long isAlignedTimerStart;
 
 	public static enum IntakeControlState {
 		Suck, // Pull in the Cube
@@ -73,12 +78,11 @@ public class Intake extends Subsystem implements LoopingSubsystem {
 				setSpeed(Constants.SPIT_SPEED, -Constants.SPIT_SPEED);
 				break;
 			case Align:
-				// setSpeed(0, 0);
 				setAlignMotors();
 				break;
 			case Idle:
-				 setSpeed(0, 0);
-//				updateSpeed();
+				setSpeed(0, 0);
+				// updateSpeed();
 				break;
 			default:
 				break;
@@ -117,6 +121,7 @@ public class Intake extends Subsystem implements LoopingSubsystem {
 		this.controlState = IntakeControlState.Idle;
 
 		this.wantedTensionPower = 0;
+		this.isAlignedTimerStart = 0;
 	}
 
 	private void setCurrentLimit(TalonSRX talon, int max, int nominal, int time) {
@@ -181,7 +186,7 @@ public class Intake extends Subsystem implements LoopingSubsystem {
 		this.wantedRightSpeed = rightSpeed;
 	}
 
-	private double getSensorDistance(double voltage) {
+	private double getDistance(double voltage) {
 		return 27.86 * Math.pow(voltage, -1.15);
 	}
 
@@ -199,7 +204,6 @@ public class Intake extends Subsystem implements LoopingSubsystem {
 	// return angle;
 	// }
 	public boolean isAligned() {
-		double threshold = 0.3;
 
 		double leftDistance = this.leftDistance.getVoltage();
 		double rightDistance = this.rightDistance.getVoltage();
@@ -208,28 +212,34 @@ public class Intake extends Subsystem implements LoopingSubsystem {
 		SmartDashboard.putNumber("leftDistance", leftDistance);
 		SmartDashboard.putNumber("rightDistance", rightDistance);
 		SmartDashboard.putNumber("distanceDifference", leftDistance - rightDistance);
-
-		return Math.abs(distanceDifference) < threshold;
+		boolean distanceThreshold = Math.abs(distanceDifference) < 0.1;
+		boolean isAligned = false;
+		if (distanceThreshold && this.isAlignedTimerStart == 0) {
+			this.isAlignedTimerStart = System.currentTimeMillis();
+		} else if (distanceThreshold && !(this.isAlignedTimerStart == 0)
+				&& System.currentTimeMillis() - this.isAlignedTimerStart > 0.1D) {
+			this.isAlignedTimerStart = 0;
+			isAligned = true;
+		}
+		return isAligned;
 
 	}
 
-	private void setAlignMotors() {
-		double multiplier = 0.2;
-		double speed = Constants.SUCK_SPEED * multiplier;
-		Intake.getInstance().setWantedSpeed(speed, speed);
+	protected void setAlignMotors() {
+		this.setSpeed(Constants.ROTATION_SPEED, Constants.ROTATION_SPEED);
 	}
 
 	public void intakeDecisions() {
 		DigitalInput front_beam = new DigitalInput((int) Math.random() * 20);
 		DigitalInput back_beam = new DigitalInput((int) Math.random() * 20);
 		if (!front_beam.get() && !back_beam.get()) { // No box in, go find one
-			setSpeed(0, 0);
+			this.setSpeed(0, 0);
 		} else if (!front_beam.get() && back_beam.get()) { // Impossible
 			System.out.println("ERROR! YOU ARE THE WEAKEST LINK!");
 		} else if (front_beam.get() && !back_beam.get()) { // The box is going in
-			setSpeed(Constants.SUCK_SPEED, -Constants.SUCK_SPEED);
+			this.setSpeed(Constants.SUCK_SPEED, -Constants.SUCK_SPEED);
 		} else if (front_beam.get() && back_beam.get()) { // You're great
-			setSpeed(0, 0);
+			this.setSpeed(0, 0);
 		}
 		return;
 	}
@@ -241,7 +251,7 @@ public class Intake extends Subsystem implements LoopingSubsystem {
 
 	@Override
 	protected void initDefaultCommand() {
-		// this.setDefaultCommand(new IntakeIdle());
+		// this.setDefaultCommand(new IntakeAlign());
 	}
 
 }
