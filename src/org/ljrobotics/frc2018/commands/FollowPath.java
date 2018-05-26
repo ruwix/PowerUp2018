27a1a6ff01ba27bfddde5fe32a25bad7162a14c3
ruwix <grantpauker@gmail.com>
2018-05-26 12:31:39 -0700

    Add follow path option

--- src/org/ljrobotics/frc2018/commands/FollowPath.java
+++ src/org/ljrobotics/frc2018/commands/FollowPath.java
@@ -1,5 +1,6 @@
 package org.ljrobotics.frc2018.commands;
 
+import org.ljrobotics.frc2018.Constants;
 import org.ljrobotics.frc2018.state.RobotState;
 import org.ljrobotics.frc2018.subsystems.Drive;
 import org.ljrobotics.lib.util.DriveSignal;
@@ -16,31 +17,39 @@ public class FollowPath extends Command {
 	private PathContainer pathContainer;
 	private Path path;
 	private long startTime;
-	
+
 	public FollowPath(PathContainer pathContainer) {
 		this.pathContainer = pathContainer;
 		this.path = this.pathContainer.buildPath();
 		this.requires(Drive.getInstance());
 	}
-	
+
 	@Override
 	protected void initialize() {
 		startTime = System.currentTimeMillis();
-		SmartDashboard.putNumber("starttime", startTime );
+		SmartDashboard.putNumber("starttime", startTime);
+		if (this.pathContainer.isInverted()) {
+			Drive.getInstance().setInverted(false);
+			Constants.GYRO_MODIFIER = -1;
+		}
 		Drive.getInstance().setWantDrivePath(path, pathContainer.isReversed());
 	}
-	
+
 	@Override
 	protected boolean isFinished() {
 		return Drive.getInstance().isDoneWithPath();
 	}
-	
+
 	@Override
 	protected void end() {
-		SmartDashboard.putNumber("totaltime", (startTime-System.currentTimeMillis())/1000D);
-		Drive.getInstance().setOpenLoop(new DriveSignal(0,0));
+		SmartDashboard.putNumber("totaltime", (startTime - System.currentTimeMillis()) / 1000D);
+		Drive.getInstance().setOpenLoop(new DriveSignal(0, 0));
+		if (this.pathContainer.isInverted()) {
+			Drive.getInstance().setInverted(true);
+			Constants.GYRO_MODIFIER = 1;
+		}
 	}
-	
+
 	@Override
 	protected void execute() {
 	}
